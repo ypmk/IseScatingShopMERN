752bbcad572b695b93c26c039314ff5e03196697
ypmk <kopylovaksusha123@gmail.com>
2024-05-16 23:52:46 +0300

    Добавлен адаптив и бургер-меню для левой панели администратора

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -5,6 +5,7 @@ import "../globals.css";
 import { ClerkProvider } from '@clerk/nextjs';
 
 import LeftSideBar from "@/components/layout/LeftSideBar";
+import TopBar from "@/components/layout/TopBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <div className="flex max-lg:flex-col text-grey-1">
             <LeftSideBar />
+            <TopBar />
             <div className="flex-1">{children}</div>
           </div>
         </body>
--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -7,15 +7,16 @@ import React from 'react'
 
 const LeftSideBar = () => {
   return (
-    <div className='h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden'>
+    <div className="h-screen left-0 top-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden">
       <Image src="/logo.jpg" alt='logo' width={150} height={70} />
-      <div className='flex flex-col gap-12'>
+      
+      <div className="flex flex-col gap-12">
         {navLinks.map(((link) =>
-          <Link href={link.url} key={link.label} className='flex gap-4 text-body-medium'>{link.icon}<p>{link.label}</p></Link>
+          <Link href={link.url} key={link.label} className="flex gap-4 text-body-medium">{link.icon}<p>{link.label}</p></Link>
         ))}
       </div>
 
-      <div className='flex gap-4 text-body-medium items-center'>
+      <div className="flex gap-4 text-body-medium items-center">
         <UserButton />
         <p> Edit Profile</p>
       </div>
