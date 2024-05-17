afb50f7ee2a320c23acad9eb3534c0490edc4749
ypmk <kopylovaksusha123@gmail.com>
2024-05-17 20:44:01 +0300

    Добавлена строка поиска категорий по названиям на странице категорий

--- app/(dashboard)/collections/page.tsx
+++ app/(dashboard)/collections/page.tsx
@@ -2,6 +2,9 @@
 
 import { columns } from '@/components/collections/CollectionColumns'
 import { DataTable } from '@/components/cusctom ui/DataTable'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { Plus } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 
 const Collections = () => {
@@ -25,11 +28,19 @@ const Collections = () => {
         getCollections()
     },[])
 
-    console.log(collections)
+   
 
   return (
-    <div>
-        <DataTable columns={columns} data={collections} />
+    <div className="px-10 py-5">
+    <div className="flex items-center justify-between">
+        <p className="text-heading2-bold">Категории</p>
+        <Button className="bg-blue-1 text-white">
+            <Plus className="h-4 w-4 mr-2"/>
+            Создать категорию
+        </Button>
+    </div>
+        <Separator className="bg-grey-1 my-4"/>
+        <DataTable columns={columns} data={collections} searchKey={"title"} />
     </div>
   )
 }
